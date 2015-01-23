36c5f0e8496dbe54a6e429bcc2973b28db05cb98
wilfriedCyrille <nowilfried@gmail.com>
2015-01-22 21:06:45 -0500

    premier commit

---
+++ src/hello_wilfrid/Hello_Wilfrid.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_wilfrid;
+
+/**
+ *
+ * @author wifrid
+ */
+public class Hello_Wilfrid {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("bonjour moi mm");
+        // TODO code application logic here
+    }
+    
+}
